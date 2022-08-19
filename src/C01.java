7cc90ecb8843a3981cffbe78fc4729ab29e4a390
“User <musagurbuz4661@gmail.com>
2022-08-19 12:49:51 +0200

    v5

--- src/C01.java
+++ src/C01.java
@@ -4,6 +4,7 @@ public class C01 {
         System.out.println("2.Deneme");
         System.out.println("Musa branch");
         System.out.println(" 4 deneme musa ");
+        System.out.println("5 deneme");
 
     }
 }
